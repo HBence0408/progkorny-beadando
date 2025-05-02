b57d298d50e9ca5b6e34b9e8a4af1900984380dd
HBence0408 <61012730+HBence0408@users.noreply.github.com>
2025-05-02 12:49:31 +0200

    Initial commit

---
+++ porgKorny_WPF_beadando/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace porgKorny_WPF_beadando
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
